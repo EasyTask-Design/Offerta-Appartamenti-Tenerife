e0c199fa91c0c9df5adb0be9c19b90e57f2ec8f2
EasyTask-Design <michelepertutto@gmail.com>
2025-12-19 09:59:13 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Offerta-Appartamenti-Tenerife/', // <--- AGGIUNGI QUESTA RIGA ESATTA
-})import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Offerta-Appartamenti-Tenerife/', // <--- AGGIUNGI QUESTA RIGA ESATTA
+  base: '/Offerta-Appartamenti-Tenerife/',
 })
